071ba27ba456741f0ba8d2fa890331447996b28a
edgarmsilva <edgar.msilva@gmail.com>
2024-03-11 16:15:31 -0300

    updates

--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/');
-  await page.getByLabel('Pesquisar', { exact: true }).click();
-  await page.getByLabel('Pesquisar', { exact: true }).fill('teste');
-  await page.getByText('Aproximadamente 1.280.000.000 resultados (0,24 segundos)');
-});
